2eb22d9b45c71d36ca10b6a9e161ae0dd0aa2acb
jevilstail <100316907+commitblue@users.noreply.github.com>
2022-10-01 21:49:57 +0200

    sasdsd ok

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const {Client, GatewayIntentBits} = require("discord.js")
+const fs = require("fs")
 require("dotenv").config()
 const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages]
 })
 let commands = {}
+fs.readdir("./commands/", (err, files) => {
+    if (err){
+        console.log(err)
+    } else {
+        files.forEach(file => {
+            const required = require(file)
+            commands[required.data.name] = required
+        })
+    }
+})
 client.on("interactionCreate", interaction => {
-    
+
 })
 client.on("ready", () => {
 
