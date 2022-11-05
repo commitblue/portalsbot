e18ddb37959b98f6ef42b02ca82816470c4b89c3
jevilstail <100316907+commitblue@users.noreply.github.com>
2022-11-05 18:29:24 +0200

    ok

--- commands/updateCache.ts
+++ commands/updateCache.ts
@@ -1,4 +1,13 @@
 import { CommandInteraction } from "discord.js"
 module.exports = {
-    
+    data: {
+        name : "updateCache",
+        description : "updates the cache"
+    },
+    command : async (interaction : CommandInteraction) => {
+        if (interaction.member?.user.id === ""){
+            await interaction.guild?.fetch()
+            await interaction.reply("Updated cache.")
+        }
+    }
 }
