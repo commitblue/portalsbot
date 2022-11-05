daefbb1182550859fe3db37bb5a9afdac3783dba
jevilstail <100316907+commitblue@users.noreply.github.com>
2022-11-05 17:58:40 +0200

    s

---
+++ commands/hi.ts
@@ -0,0 +1,11 @@
+import { CommandInteraction } from "discord.js";
+
+module.exports = {
+    data : {
+        name : "hi",
+        description : "Says hi back."
+    },
+    command : async (interaction : CommandInteraction) => {
+       
+    }
+}
