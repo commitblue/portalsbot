5cd1c072bb5347419c6be280973ce0250c2249fb
jevilstail <100316907+commitblue@users.noreply.github.com>
2022-11-25 23:10:20 +0200

    s

--- commands/cyrptography.ts
+++ commands/cyrptography.ts
@@ -1,5 +1,5 @@
 import { CommandInteraction, SlashCommandBuilder } from "discord.js";
-
+import cyrptoAES from "crypto-js/aes"
 module.exports = {
     data: new SlashCommandBuilder()
     .setName("cyrptography")
@@ -8,11 +8,21 @@ module.exports = {
         command
         .setName("aesonetwoeightencrypt")
         .setDescription("uses aes128 encoder to encrypt")
+        .addStringOption(option => option
+            .setName("value")
+            .setDescription("value")
+        )
+        .addStringOption(option => option
+            .setName("blob")
+            .setDescription("blob")
+        )
     ),
     async execute(interaction : CommandInteraction){
         if (!interaction.isChatInputCommand()){return} // ts === good
+        const val = interaction.options.getString("value")
+        const blob = interaction.options.getString("blob")
         if (interaction.options.getSubcommand() === "aesonetwoeightEncrypt"){
-            await interaction.reply("w")
+            await interaction.reply(cyrptoAES.encrypt())
         }
     }
 }
