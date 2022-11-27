5afba8cbcf27c07a4fd40b6ca9ad7e4a9e3d1c02
jevilstail <100316907+commitblue@users.noreply.github.com>
2022-11-27 15:32:58 +0200

    s

--- commands/cyrptography.ts
+++ commands/cyrptography.ts
@@ -21,10 +21,10 @@ module.exports = {
     ),
     async execute(interaction : CommandInteraction){
         if (!interaction.isChatInputCommand()){return} // ts === good
-        const val : string | null = interaction.options.getString("value")
-        const blob : string | null = interaction.options.getString("blob")
-        if (!val || !blob){return await interaction.reply("no")}
         if (interaction.options.getSubcommand() === "aesonetwoeightEncrypt"){
+            const val : string | null = interaction.options.getString("value")
+            const blob : string | null = interaction.options.getString("blob")
+            if (!val || !blob){return await interaction.reply("no")}
             await interaction.reply(cyrptojs.AES.encrypt(val, blob).toString())
         }
     }
